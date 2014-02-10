ea19ea49f3a13faab640a781db54a5e4c9e66015
xyzelement <ed.markovich@gmail.com>
2014-02-09 22:50:20 -0500

    moved days code into its own js

---
+++ client/Days.js
@@ -0,0 +1,31 @@
+function dateToKey(date) {
+  //EMTODO: avoid duplicating this function
+  return date.format("YYYYMMDD");
+}
+
+function makeDayButton(day) {
+  moment.lang('en', {
+  	        'calendar' : {
+            'lastDay' : '[Yesterday]',
+            'sameDay' : '[Today]',
+            'lastWeek' : 'dddd',
+            'nextWeek' : 'dddd',
+            'sameElse' : 'dddd'
+       }
+  });
+  return {text: "<b>" + day.format("MMM D")+"</b> " +day.calendar(), date: dateToKey(day)};
+}
+
+Template.days.buttons = function () {
+  var out = [];
+  for (var i=0; i<7; ++i){
+    out.push( makeDayButton(moment().subtract('days', i) ) );
+  }
+
+  return out;
+}
+
+Template.days.events({'click ': function(event,template) {
+  var date = event.target.getAttribute("data-id");  
+  Session.set('lastUpdate', date );
+}});
--- client/HabitScore.js
+++ client/HabitScore.js
@@ -62,34 +62,8 @@ function dateToKey(date) {
   return date.format("YYYYMMDD");
 }
 
-function makeDayButton(day) {
-    moment.lang('en', {
-        'calendar' : {
-            'lastDay' : '[Yesterday]',
-            'sameDay' : '[Today]',
-            'lastWeek' : 'dddd',
-            'nextWeek' : 'dddd',
-            'sameElse' : 'dddd'
-       }
-    });
 
-  return {text: "<b>" + day.format("MMM D")+"</b> " +day.calendar(), date: dateToKey(day)};
-}
 
-Template.days.buttons = function () {
-  var out = [];
-  for (var i=0; i<7; ++i){
-    out.push( makeDayButton(moment().subtract('days', i) ) );
-  }
-
-  console.log(out);
-  return out;
-}
-
-Template.days.events({'click ': function(event,template) {
-  var date = event.target.getAttribute("data-id");  
-  Session.set('lastUpdate', date );
-}});
 
 Meteor.startup(function(){
  Session.set('lastUpdate', dateToKey(moment()) );
