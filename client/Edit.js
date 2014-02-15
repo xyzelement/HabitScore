88ffa9b197fab5ddf4677d0790567272f4d1dc20
xyzelement <ed.markovich@gmail.com>
2014-02-15 11:24:35 -0500

    fixed out of synch stupid checkboxes

--- client/Edit.js
+++ client/Edit.js
@@ -1,29 +1,34 @@
+
+
+
 Template.edit.events({'click .edit-save': function(event,template) {
   //EMTODO: input validation 
 
-  var habit        = $('#edit-habit').val();
-  
-  var badges = {}
 
-  badges.use_good_badge = $('#use_good_badge').attr('checked');
-  badges.good           = $('#edit-good-badge').val();
-  badges.good_days1     = $('#edit-good-days1').val();
-  badges.good_days2     = $('#edit-good-days2').val();
+  var habit = Session.get("currentlyEdited");
+  habit.name        = $('#edit-habit').val();
   
-  badges.use_bad_badge  = $('#use_bad_badge').attr('checked');
-  badges.bad            = $('#edit-bad-badge').val();  
-  badges.bad_days1      = $('#edit-bad-days1').val();
-  badges.bad_days2      = $('#edit-bad-days2').val();
+  habit.badges.use_good_badge = $('#use_good_badge').attr('checked');
+  habit.badges.good           = $('#edit-good-badge').val();
+  habit.badges.good_days1     = $('#edit-good-days1').val();
+  habit.badges.good_days2     = $('#edit-good-days2').val();
   
-  console.log(JSON.stringify(badges));
+  habit.badges.use_bad_badge  = $('#use_bad_badge').attr('checked');
+  habit.badges.bad            = $('#edit-bad-badge').val();  
+  habit.badges.bad_days1      = $('#edit-bad-days1').val();
+  habit.badges.bad_days2      = $('#edit-bad-days2').val();
   
-  var id = Session.get("currentlyEdited")._id;
-  Habits.update({_id: id}, {$set: {name: habit}});
-  Habits.update({_id: id}, {$set: {badges: badges}});
+  //EMTODO: can this be a single DB op?
+
+  Habits.update({_id: habit._id}, {$set: {name: habit.name}});
+  Habits.update({_id: habit._id}, {$set: {badges: habit.badges}});
+
+  Session.set("currentlyEdited",habit);
 }});
 
 Template.edit.selected = function () {
-  return Session.get("currentlyEdited");
+  var sel = Session.get("currentlyEdited");
+  return sel;
 };
 
 Handlebars.registerHelper('habit_checkbox', function(habit, which) {
@@ -32,20 +37,20 @@ Handlebars.registerHelper('habit_checkbox', function(habit, which) {
 
 
   switch(which) {
-    case "good":
-      label   = "Award a badge for performing this habbit";
+    case "good":      
       checked = habit && habit.badges && habit.badges.use_good_badge ? " checked" : "";
+      label   = "Badge of Awesome!";
       id      = "use_good_badge";
       break;
     case "bad" :
-      label   = "badge of SHAME!";
       checked = habit && habit.badges && habit.badges.use_bad_badge ? " checked" : "";
+      label   = "Badge of Shame!";      
       id      = "use_bad_badge";
       break;   
     default:
       console.log("Unknown which:"+which+":");
   }
-  
+
   return '<div class="checkbox"><label><input type="checkbox" id="'
           +id+'"'+checked+'>'+label+'</label></div>'
 });
