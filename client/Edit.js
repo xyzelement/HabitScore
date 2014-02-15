0eed9b24e77b47dfb2d8ea69882785a5897749d2
xyzelement <ed.markovich@gmail.com>
2014-02-14 22:54:59 -0500

    save checkbox status

--- client/Edit.js
+++ client/Edit.js
@@ -4,13 +4,16 @@ Template.edit.events({'click .edit-save': function(event,template) {
   var habit        = $('#edit-habit').val();
   
   var badges = {}
-  badges.good         = $('#edit-good-badge').val();
-  badges.good_days1   = $('#edit-good-days1').val();
-  badges.good_days2   = $('#edit-good-days2').val();
+
+  badges.use_good_badge = $('#use_good_badge').attr('checked');
+  badges.good           = $('#edit-good-badge').val();
+  badges.good_days1     = $('#edit-good-days1').val();
+  badges.good_days2     = $('#edit-good-days2').val();
   
-  badges.bad         = $('#edit-bad-badge').val();  
-  badges.bad_days1   = $('#edit-bad-days1').val();
-  badges.bad_days2   = $('#edit-bad-days2').val();
+  badges.use_bad_badge  = $('#use_bad_badge').attr('checked');
+  badges.bad            = $('#edit-bad-badge').val();  
+  badges.bad_days1      = $('#edit-bad-days1').val();
+  badges.bad_days2      = $('#edit-bad-days2').val();
   
   console.log(JSON.stringify(badges));
   
@@ -22,6 +25,30 @@ Template.edit.events({'click .edit-save': function(event,template) {
 Template.edit.selected = function () {
   return Session.get("currentlyEdited");
 };
+
+Handlebars.registerHelper('habit_checkbox', function(habit, which) {
+  var label;
+  var checked;
+
+
+  switch(which) {
+    case "good":
+      label   = "Award a badge for performing this habbit";
+      checked = habit && habit.badges && habit.badges.use_good_badge ? " checked" : "";
+      id      = "use_good_badge";
+      break;
+    case "bad" :
+      label   = "badge of SHAME!";
+      checked = habit && habit.badges && habit.badges.use_bad_badge ? " checked" : "";
+      id      = "use_bad_badge";
+      break;   
+    default:
+      console.log("Unknown which:"+which+":");
+  }
+  
+  return '<div class="checkbox"><label><input type="checkbox" id="'
+          +id+'"'+checked+'>'+label+'</label></div>'
+});
 
 Handlebars.registerHelper('days_selector', function(id, selected) {
 
